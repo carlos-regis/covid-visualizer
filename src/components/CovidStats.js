4d3ced3c13c669acab5c4fc559ad4b83b4714e71
Carlos Regis <cr@carlosregis.com>
2023-05-02 22:28:30 +0000

    add countries navbar

--- src/components/CovidStats.js
+++ src/components/CovidStats.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+function CountriesNav({ selectedCountry, onUpdateCountry }) {
+  const countries = ['All', 'USA', 'India', 'France', 'Germany', 'Brazil', 'Japan', 'Italy', 'UK'];
+
+  return (
+    <select 
+      onChange={(event) => onUpdateCountry(event.target.value)}
+      selected={selectedCountry}
+    >
+      {countries.map((country) => (
+        <option key={country} value={country}>
+          {country}
+        </option>
+      ))}
+    </select>
+  );
+}
+
 class CovidStats extends React.Component {
   constructor(props) {
     super(props);
@@ -13,23 +30,19 @@ class CovidStats extends React.Component {
 
   updateCountry(selectedCountry) {
     this.setState({
-      selectedCountry: selectedCountry,
+      selectedCountry,
     });
   }
-  
+
   render() {
-    const countries = ['All', 'USA', 'India', 'France', 'Germany', 'Brazil', 'Japan', 'Italy', 'UK'];
+    const { selectedCountry } = this.state;
+
     return (
       <main>    
-        <select 
-          onChange={(event) => this.updateCountry(event.target.value)}
-          selected={this.state.selectedCountry}>
-          {countries.map((country) => (
-            <option key={country} value={country}>
-              {country}
-            </option>
-          ))}
-        </select>
+        <CountriesNav 
+          selected={selectedCountry}
+          onUpdateCountry={this.updateCountry}
+        />
         {JSON.stringify(this.state.selectedCountry, null, 2)}
       </main>
     );
