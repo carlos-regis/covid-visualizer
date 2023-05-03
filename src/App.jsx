b6f699ddac7c7424c30112f13861360fcdb7aba2
Carlos Regis <cr@carlosregis.com>
2023-05-03 17:09:25 +0000

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import CovidStats from './components/CovidStats';
 import Comparison from './components/Comparison';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-      <div className='light'>
-        <div className="container">
-          <CovidStats />
-        </div>
-      </div>
-  );
+    return (
+        <Router>
+            <div className="light">
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<CovidStats />} />
+                        <Route path="/comparison" element={<Comparison />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
