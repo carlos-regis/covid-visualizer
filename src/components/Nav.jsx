bd86648f5857940e62724e55eb28740814a371e6
Carlos Regis <cr@carlosregis.com>
2023-05-03 21:44:53 +0000

    add production build

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ function Nav({ theme, toggleTheme }) {
                     'nav-link' + (isActive ? ' active' : '')
                 }
             >
-                Covid Visualiser
+                Covid Visualizer
             </NavLink>
             <ul className="row">
                 <li>
